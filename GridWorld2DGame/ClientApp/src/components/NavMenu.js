d81e37237334062eb01dc09b0e940be076b2ddfc
Florabel Ituralde <florabel.ituralde@outlook.com>
2023-02-28 10:42:48 -0500

    removed extra nav menu

--- GridWorld2DGame/ClientApp/src/components/NavMenu.js
+++ GridWorld2DGame/ClientApp/src/components/NavMenu.js
@@ -35,9 +35,6 @@ export class NavMenu extends Component {
               <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/board-game">Board Game</NavLink>
               </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/board-game">Board Game</NavLink>
-              </NavItem>
             </ul>
           </Collapse>
         </Navbar>
